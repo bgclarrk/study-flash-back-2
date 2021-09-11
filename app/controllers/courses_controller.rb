e695544ef53ee880a51a0dd128dd5e82817d0562
+ <+>
2021-09-11 13:34:39 -0700

    Adding a new card to the DOM

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,5 +4,16 @@ class CoursesController < ApplicationController
         courses = Course.all
         render json: courses
     end
+
+    def create
+        course = Course.create(course_params)
+        render json: course
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:name, :description)
+    end
     
 end
